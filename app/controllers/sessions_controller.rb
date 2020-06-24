05b38b3dd2aad1726086450db33710455c81f607
EdgarSalasV <edgardre01@gmail.com>
2020-06-24 16:35:26 -0500

    init, rails with devise-jwt

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsCOntroller < Devise::SessionsCOntroller
+  respond_to :json
+
+  private
+
+  def respond_with(ressource, _opts = {})
+    render json: resource
+  end
+
+  def respond_to_destroy
+    head :no_content
+  end
+end
